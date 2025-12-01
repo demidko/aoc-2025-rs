1532299c1c990bb44bdeb6d4bdcc118442fa3ff4
Daniil Demidko <demidko@farpost.com>
2025-11-30 22:40:49 -0300

    preparations

--- src/d1.rs
+++ src/d1.rs
@@ -1,3 +1,3 @@
-pub fn print_d1_answers() {
+pub fn print_d1() {
     todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::d1::print_d1_answers;
+use crate::d1::print_d1;
 
 mod d1;
 
 fn main() {
-    print_d1_answers();
+    print_d1();
 }
