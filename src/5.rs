81df14646b61d0f3f0f66974f918d75dfd848123
Daniil Demidko <demidko@farpost.com>
2025-12-05 18:35:05 -0300

    Day 5: Cafeteria

--- src/5.rs
+++ src/5.rs
@@ -23,31 +23,24 @@ fn day_5_cafeteria_part_two() {
     use std::cmp::max;
     use std::iter::once;
     use std::ops::RangeInclusive;
-
     type Range = RangeInclusive<u64>;
 
-    let input = include_str!("../5/input");
-    let (ranges_txt, _) = input.split_once("\n\n").unwrap();
-    let get_start = |r: &Range| *r.start();
-    let get_end = |r: &Range| *r.end();
-    let calc_len = |r: &Range| get_end(r) - get_start(r) + 1;
-    let is_connected = |prev: &Range, next: &Range| get_start(next) <= get_end(prev) + 1;
-    let calc_union = |prev: &Range, next: &Range| get_start(prev)..=max(get_end(prev), get_end(next));
-    let partition_vec = |vec: Vec<Range>, pivot: &Range| vec.into_iter().partition(|r| get_start(r) < get_start(pivot));
+    let (ranges_txt, _) = include_str!("../5/input").split_once("\n\n").unwrap();
+    let is_connected = |prev: &Range, next: &Range| *next.start() <= *prev.end() + 1;
+    let calc_union = |prev: &Range, next: &Range| *prev.start()..=max(*prev.end(), *next.end());
+    let partition_vec = |vec: Vec<Range>, pivot: &Range| vec.into_iter().partition(|r| *r.start() < *pivot.start());
     let take_head = |vec: Vec<Range>| vec.split_last().map(|(_, head)| head.to_vec()).unwrap_or_default();
     let append_to_vec = |vec: Vec<Range>, item: Range| vec.into_iter().chain(once(item)).collect();
     let join_mid = |(left, right): (Vec<Range>, Vec<Range>), mid: Range| left.into_iter().chain(once(mid)).chain(right).collect();
     let insert_ordered = |vec: Vec<Range>, item: Range| join_mid(partition_vec(vec, &item), item);
-    let replace_last = |vec: Vec<Range>, item: Range| append_to_vec(take_head(vec), item);
-    let replace_last_rev = |item: Range, vec: Vec<Range>| replace_last(vec, item);
-    let sort_step = |acc, next| insert_ordered(acc, next);
+    let replace_last = |item: Range, vec: Vec<Range>| append_to_vec(take_head(vec), item);
     let check_merge = |acc: &Vec<Range>, next: &Range| acc.last().filter(|last| is_connected(last, next)).is_some();
     let get_union_item = |acc: &Vec<Range>, next: &Range| acc.last().map(|last| calc_union(last, next)).unwrap();
-    let branch_merge = |acc: Vec<Range>, next: Range| replace_last_rev(get_union_item(&acc, &next), acc);
-    let branch_append = |acc: Vec<Range>, next: Range| append_to_vec(acc, next);
-    let merge_step = |acc: Vec<Range>, next: Range| check_merge(&acc, &next).then(|| branch_merge(acc.clone(), next.clone())).unwrap_or_else(|| branch_append(acc, next));
+    let branch_merge = |acc: Vec<Range>, next: Range| replace_last(get_union_item(&acc, &next), acc);
+    let merge_step = |acc: Vec<Range>, next: Range| check_merge(&acc, &next).then(|| branch_merge(acc.clone(), next.clone())).unwrap_or_else(|| append_to_vec(acc, next));
     let parse_line = |s: &str| s.split_once('-').map(|(a, b)| a.parse::<u64>().unwrap()..=b.parse::<u64>().unwrap()).unwrap();
-    let total = ranges_txt.lines().map(parse_line).fold(Vec::new(), sort_step).into_iter().fold(Vec::new(), merge_step).iter().map(calc_len).sum::<u64>();
+    let calc_len = |r: &Range| r.end() - *r.start() + 1;
+    let total = ranges_txt.lines().map(parse_line).fold(Vec::new(), insert_ordered).into_iter().fold(Vec::new(), merge_step).iter().map(calc_len).sum::<u64>();
 
     println!("{}", total);
 }
